2c2d37403bfd2ef39d69eac6e4e77710a9f75949
jcwoan <106583453+jcwoan@users.noreply.github.com>
2022-05-31 17:26:31 +0900

    Add files via upload

---
+++ Youtube_edu/Containers.py
@@ -0,0 +1,15 @@
+# Containers
+my_list1 = [10,10.1,'python',True]
+print(my_list1)
+print(type(my_list1))
+
+my_list2 = list([1,2,3]) # LIST함수로 값을 넣어줌
+print(my_list2)
+
+
+my_tuple1 = (10,10.1,'python',True)
+print(my_tuple1)
+print(type(my_tuple1))
+
+my_tuple2 = tuple([1,2,3]) # tuple함수로 중괄호안에 list형식으로 값을 넣어줌
+print(my_tuple2)
---
+++ Youtube_edu/Variables.py
@@ -0,0 +1,22 @@
+ 
+#Bitwise 연산자
+print(bin(2)) #십진법 -> 이진법 변환
+c=1+2j
+print(c)
+ 
+# Methods 함수
+c=1+2j
+print(type(c))
+
+print(c.conjugate()) #부호변경
+
+print(c)
+
+print(c.real) #
+
+print(c.imag) #
+
+name = 'kevin'
+print(name.capitalize())#첫글자 대문자료변환
+
+print(name.split('v')) #글자을 나눔
